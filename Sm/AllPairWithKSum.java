17dfa007ae3980bee9325f3b1fb285b55b1e6dd3
Flop <41886233+megaflops@users.noreply.github.com>
2018-08-01 14:51:22 +0530

    Final version

--- Sm/AllPairWithKSum.java
+++ Sm/AllPairWithKSum.java
@@ -12,7 +12,7 @@ class AllPairWithKSum {
 			System.out.println(aInt[i]);
 		}
     
-    ArrayList<List<Integer>> retList = paisWithKSum(aInt, aInt.length, 11);
+    ArrayList<List<Integer>> retList = findPairWithKSum(aInt, aInt.length, 11);
 
     for (List a : retList) {
         System.out.println(a.get(0) + " " + a.get(1));
@@ -20,18 +20,22 @@ class AllPairWithKSum {
     
     }
 
-    public static ArrayList<List<Integer>> paisWithKSum(int[] arr,int len,int k) {
+    public static ArrayList<List<Integer>> findPairWithKSum(int[] arr,int len,int k) {
         ArrayList<List<Integer>> retList = new ArrayList<List<Integer>>(); 
+        List<Integer> lst = new ArrayList<Integer>() ;
+        
         for(int i=0;i<len;i++) {
-            for(int j=0;j<len;j++) {
-                if((arr[i]+ arr[j]) ==k ) {
-                    List<Integer> tmp = new ArrayList<Integer>();
-                    tmp.add(arr[i]);
-                    tmp.add(arr[j]);
-                    retList.add(tmp);
-                                    }
+            if(lst.contains( k - arr[i]) ) {
+                List<Integer> tmp = new ArrayList<Integer>();
+                tmp.add(arr[i]);
+                tmp.add(k - arr[i]);
+                retList.add(tmp);                                        
             }
-        }
+
+            lst.add(arr[i]);
+            }
+        
         return retList;
     }
+
 }
